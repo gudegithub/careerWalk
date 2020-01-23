a4eeecc737c38ff22a3f80643536ac4878fe0e53
k-iwashita <k.iwashita.gude@gmail.com>
2020-01-23 18:39:55 +0900

    出席管理機能のテスト完了

--- spec/system/events_spec.rb
+++ spec/system/events_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'イベント機能', type: :system do
   let(:test_user) { FactoryBot.create(:user) }
-  let(:event) {FactoryBot.create(:event)}
+  let(:event) {FactoryBot.create(:event, id:1, adminUser: 1)}
   let(:category) { FactoryBot.create(:category)}
   let(:user_login) do
     test_user
@@ -11,6 +11,9 @@ describe 'イベント機能', type: :system do
     fill_in 'パスワード', with: test_user.password
     click_button 'Log in'
   end
+  let(:user_logout) {
+    click_on 'ログアウト'
+  }
 
   describe 'イベント新規作成機能' do
     context 'ユーザーがログインしていない場合' do
@@ -69,5 +72,63 @@ describe 'イベント機能', type: :system do
         end
       end
     end
+  end
+
+  describe 'イベント参加受付機能' do
+    let(:user_a) { FactoryBot.create(:user, id: 1, name: 'ユーザーa', email: 'user_a@test.com') }
+    let(:user_b) { FactoryBot.create(:user, id: 2, name: 'ユーザーb', email: 'user_b@test.com') }
+
+    before do
+      event
+      visit user_session_path
+      fill_in 'Eメール', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'Log in'
+      visit event_path(1)
+      click_button 'イベントに参加する'
+    end
+
+    describe '管理ユーザーがログインしているとき' do
+      let(:login_user) { user_a }
+
+      it '出席管理画面が表示される' do
+        expect(page).to have_content '参加者名'
+      end
+
+      context '出席処理したとき' do
+        before do
+          click_on '出席'
+        end
+
+        it '出席状態になる' do
+          expect(page).to have_content '出席取り消し'
+        end
+      end
+
+      context '管理ユーザーがログアウトしたとき' do
+        before do
+          click_on 'ログアウト'
+        end
+
+        let(:login_user) { user_b }
+
+        it '出席管理画面が表示されない' do
+          expect(page).to have_no_content '参加者名'
+        end
+      end
+
+    end
+
+    describe '管理者以外のユーザーがログインしているとき' do
+      let(:login_user) { user_b }
+      before do
+        visit event_path(1)
+      end
+
+      it '出席管理画面が表示されない' do
+        expect(page).to have_no_content '参加者名'
+      end
+    end
+
   end
 end
